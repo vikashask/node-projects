884eade1a500bc0b490ff0d02b2d05a9a7315d4a
vikash <vikashraj144@gmail.com>
2021-06-20 08:20:48 +0530

    ui changes

--- node_mongo_rest_feed_react_app/ui/src/components/Logo/Logo.js
+++ node_mongo_rest_feed_react_app/ui/src/components/Logo/Logo.js
@@ -1,7 +1,6 @@
-import React from 'react';
+import React from "react";
+import "./Logo.css";
 
-import './Logo.css';
-
-const logo = props => <h1 className="logo">MessageNode</h1>;
+const logo = (props) => <h1 className="logo">Feed management</h1>;
 
 export default logo;
